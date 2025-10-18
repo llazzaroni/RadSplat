3fb669f7ca13b0f996a860f2656f22ff9ace4598
riccardobollati <riccardo.bollati.01@gmail.com>
2025-10-18 13:09:02 +0200

    test1

--- gs_initialization.py
+++ gs_initialization.py
@@ -1,15 +1,30 @@
-from nerfstudio.utils.eval_utils import eval_setup
+from pathlib import Path
+
+from submodules.nerfstudio.nerfstudio.utils.eval_utils import eval_setup
 
 class InitializationPipeline:
 
     def __init__(self, checkpoint_folder: str):
 
-        self.model_folder = checkpoint_folder
-
+        self.checkpoint_folder = Path(checkpoint_folder)
+        self.pipeline, self.model = self._load_model() 
+        self._load_model()
 
         pass
 
     def _load_model(self):
-        pipeline, _, _ = eval_setup(self.model_folder, test_mode="test")
+        _, pipeline, _, _ = eval_setup(self.checkpoint_folder, test_mode="test")
+
+        return pipeline, pipeline.model
+
+    def render_camera(self):
+        if self.pipeline.datamanager.train_dataset:
+            camera = self.pipeline.datamanager.train_dataset[0]["image_idx"]  # or a custom camera
+            outputs = self.model.get_outputs_for_camera(camera)
+            image = outputs["rgb"]
+
+            return image
+        
+        return None
 
 
