653192834dfe7bdd4e9c160859abd8a6d9d81654
riccardobollati <riccardo.bollati.01@gmail.com>
2025-10-25 16:39:57 +0200

    includeing batching

--- gs_initialization.py
+++ gs_initialization.py
@@ -1,4 +1,5 @@
 import warnings
+import math
 import torch
 
 from nerf_models import Nerfacto
@@ -13,32 +14,53 @@ warnings.filterwarnings(
 if __name__ == "__main__":
     folder = "/work/courses/dslab/team20/rbollati/running_env/outputs/poster/nerfacto/2025-10-18_013814/config.yml"
 
+    N_RAYS = 1_000_000
+    BATCH_SIZE = 5_000
+
+    n_batches = math.ceil(N_RAYS / BATCH_SIZE)
+
     
     print('########### loading model')
     model = Nerfacto(folder)
 
-    print('########### sampling rays')
-    coords = random_sampler(model.pipeline.datamanager, 1000, model.device)
-    rays = model.create_rays(coords)
+    # saple points
+    coords = random_sampler(model.pipeline.datamanager, N_RAYS, model.device)
 
-    print('########### sampling points')
+    xyzrgb_chunks = []
+    for b in range(n_batches):
+        print(f"worinng of chink {b}/{n_batches}")
 
-    sampled = model.sample_points(rays)
-    outputs, field_outputs, weights  = model.evaluate_points(sampled)
+        # get initial and final index of the index rays to query
+        s = b * BATCH_SIZE
+        e = min((b + 1) * BATCH_SIZE, N_RAYS)
+        if s >= e:
+            break
+        batch_rays_indexes = coords[s:e, :]
+        B = batch_rays_indexes.shape[0]
 
-    print('########### init gs')
-    gs_initializer = Initializer(weights, sampled)
+        print('########### sampling rays')
+        rays = model.create_rays(batch_rays_indexes)
 
-    print('########### compute trasmittance')
-    trasmittance = gs_initializer.compute_transmittance()
+        print('########### sampling points')
 
-    print('########### computer initial_position')
-    initial_position = gs_initializer.compute_inital_positions(trasmittance, 0.5)
+        sampled = model.sample_points(rays)
+        outputs, field_outputs, weights  = model.evaluate_points(sampled)
 
-    # Concatenate rgb to the positions
-    xyzrgb_batch = torch.cat([initial_position, outputs['rgb']], dim=-1)
+        print('########### init gs')
+        gs_initializer = Initializer(weights, sampled)
 
-    print('########### initial positions')
-    torch.save(xyzrgb_batch, 'test_from_refactor.pt')
+        print('########### compute trasmittance')
+        trasmittance = gs_initializer.compute_transmittance()
 
-    print(initial_position)
+        print('########### computer initial_position')
+        initial_position = gs_initializer.compute_inital_positions(trasmittance, 0.5)
+
+        xyzrgb_batch = torch.cat([initial_position, outputs['rgb']], dim=-1)
+
+        xyzrgb_chunks.append(xyzrgb_batch.detach().cpu())
+
+
+    xyzrgb = torch.cat(xyzrgb_chunks, dim=0)
+
+    print('########### saving initial positions')
+    torch.save(xyzrgb, 'big_sample.pt')
