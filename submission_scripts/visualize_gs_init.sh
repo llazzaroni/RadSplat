270f17ed7ca02455d7fff636c951f06af91387a8
Riccardo Bollati <rbollati@student-cluster2.inf.ethz.ch>
2025-10-25 18:42:21 +0200

    train instant-ngp

--- submission_scripts/visualize_gs_init.sh
+++ submission_scripts/visualize_gs_init.sh
@@ -28,26 +28,26 @@ pip install --no-build-isolation --no-binary :all: --force-reinstall \
 
 python - << 'PY'
 import torch, numpy, cv2, fused_ssim
-print("torch:", torch._version_, "cuda rt:", torch.version.cuda, "gpu:", torch.cuda.is_available())
+print("torch:", torch.__version__, "cuda rt:", torch.version.cuda, "gpu:", torch.cuda.is_available())
 if torch.cuda.is_available(): print("sm:", ".".join(map(str, torch.cuda.get_device_capability())))
-print("numpy:", numpy._version_)
-print("cv2:", cv2._version_, "has calib3d:", hasattr(cv2,"getOptimalNewCameraMatrix"))
+print("numpy:", numpy.__version__)
+print("cv2:", cv2.__version__, "has calib3d:", hasattr(cv2,"getOptimalNewCameraMatrix"))
 print("fused_ssim import OK")
 PY
 
 SCENE_DIR="/work/courses/dslab/team20/data/poster"
-RESULT_DIR="/work/courses/dslab/team20/results_gsplat_poster/nerf_init_2"
+RESULT_DIR="/work/courses/dslab/team20/results_gsplat_poster/nerf_init_1M_points"
 RENDER_TRAJ_PATH="ellipse"
 DATA_FACTOR=4
 
-REPO_DIR="/home/ds-lab/RadSplat"
+REPO_DIR="/home/rbollati/ds-lab/RadSplat"
 cd "$REPO_DIR"
 
-srun python gsplat_trainer_nerf_init.py default \
+srun python gs_splat_ckp_0.py default \
   --eval_steps -1 \
   --disable_viewer \
   --data_factor "$DATA_FACTOR" \
   --render_traj_path "$RENDER_TRAJ_PATH" \
   --data_dir "$SCENE_DIR/" \
   --result_dir "$RESULT_DIR/" \
-  --ckpt /work/courses/dslab/team20/results_gsplat_poster/nerf_init/ckpts/ckpt_7999_rank0.pt
+  --ckpt /work/courses/dslab/team20/results_gsplat_poster/nerf_init_1M_points/ckpts/ckpt_0_rank0.pt
