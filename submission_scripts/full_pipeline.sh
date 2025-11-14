f5cd328fadfb14032ec8d5b2362369fe8dc8c3cd
Lorenzo Lazzaroni <llazzaroni@student-cluster1.inf.ethz.ch>
2025-11-14 18:54:56 +0100

    modified full_pipeline

--- submission_scripts/full_pipeline.sh
+++ submission_scripts/full_pipeline.sh
@@ -31,7 +31,7 @@ export NERF_MAX_NUM_ITERATIONS=500
 export DATA_DIR="${BASE_DATA_DIR}/${SCENE}"
 export NERF_MODEL="${EXPERIMENT_DIR}/outputs/${EXPERIMENT_NAME}/nerfacto/${EXPERIMENT_NAME}/config.yml"
 
-export OUTPUT_NAME="${EXPERIMENT_DIR}/ray_sample.pt"
+export POSITION_TENSOR_OUTPUT_NAME="${EXPERIMENT_DIR}/ray_sample.pt"
 export RAY_SAMPLING_STRATEGY="canny"
 
 mkdir "${EXPERIMENT_DIR}"
@@ -57,7 +57,45 @@ ns-train nerfacto \
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] [Training-nerfacto] finished"
 
 START_SAMPLING=$(($(date +%s)*1000))
-python ~/ds-lab/RadSplat/nerf_query.py --nerf-folder $NERF_MODEL --output-name $OUTPUT_NAME --ray-sampling-strategy $RAY_SAMPLING_STRATEGY 
+python ~/ds-lab/RadSplat/nerf_query.py --nerf-folder $NERF_MODEL --output-name $POSITION_TENSOR_OUTPUT_NAME --ray-sampling-strategy $RAY_SAMPLING_STRATEGY 
 END_SAMPLING=$(($(date +%s)*1000))
 
-echo "##################### [TERMINATED] #####################"
+echo "##################### [FINISHED NERF] #####################"
+echo "##################### [STARTING GSPLAT] #####################"
+
+conda deactivate
+
+set -euo pipefail
+
+[ -f /etc/profile.d/modules.sh ] && source /etc/profile.d/modules.sh
+[ -f /usr/share/Modules/init/bash ] && source /usr/share/Modules/init/bash
+
+set +u
+module purge
+module load cuda/12.8
+module load gcc/12
+set -u
+
+set +u
+source /work/courses/dslab/team20/miniconda3/etc/profile.d/conda.sh
+conda activate gsplat-gpu
+set -u
+
+pip uninstall -y fused-ssim || true
+
+pip install --no-build-isolation --no-binary :all: --force-reinstall \
+  git+https://github.com/rahul-goel/fused-ssim@328dc9836f513d00c4b5bc38fe30478b4435cbb5
+
+RENDER_TRAJ_PATH="ellipse"
+DATA_FACTOR=4
+
+srun python save_stats.py default \
+  --nerf_init True \
+  --pt_path "$POSITION_TENSOR_OUTPUT_NAME" \
+  --save_first_ckp True \
+  --eval_steps -1 \
+  --disable_viewer \
+  --data_factor "$DATA_FACTOR" \
+  --render_traj_path "$RENDER_TRAJ_PATH" \
+  --data_dir "$DATA_DIR/" \
+  --result_dir "$EXPERIMENT_DIR/"
